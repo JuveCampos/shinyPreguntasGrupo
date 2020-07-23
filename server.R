cae8ce7f1f9584f4beea42ccd84056e6ade1ff99
Helios Omar Garcia Martinez <61509288+h3li0s123@users.noreply.github.com>
2020-07-23 13:33:17 -0500

    Update server.R
    Para el servidor agregué lo siguente:
    1) El proceso para que la App pueda leer el archivo de excel que suba el usuario (linea 5 a 20)
    2) Después, especifiqué el output correspondiente para que la App imprima el texto de la pregunta seleccionada aleatoriamente de la base de datos del usuario (línea 17 a 26)

--- server.R
+++ server.R
@@ -2,6 +2,28 @@ library(shiny)
 
 shinyServer(function(input, output) {
   
+  mydata <- reactive({
+    
+    inFile <- input$file1
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    tbl <- read_excel(inFile$datapath)
+    
+    return(tbl)
+  })
+  
+  pregunta_prueba <- eventReactive(input$btnAccion_prueba,{
+    op_prueba(data = mydata(),
+              catego = input$selCat)
+  })
+  
+  output$texto_prueba <- renderText({
+    Sys.sleep(time = 1)
+    pregunta_prueba()
+  })
+  
   pregunta_pantalla <- eventReactive(input$btnAccion,{
     op(catego = input$selCat)
   })
@@ -12,3 +34,4 @@ shinyServer(function(input, output) {
   })
   
 })
+
